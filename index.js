a3b85f2f75b404ba096f2f5e4159e41809a589b1
ANyanCatFan <saitam.spatz@gmail.com>
2017-04-23 20:11:11 +0200

    Migrate to node-telegram-bot-api

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const TeleBot = require("telebot");
-const bot = new TeleBot(process.env.API_KEY);
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.API_KEY, {
+  polling: true
+});
+//const socket = require('socket.io-client').connect("http://localhost:3000");
 const socket = require('socket.io-client').connect("https://instapapas.herokuapp.com");
 const fs = require("fs");
 
-bot.on("/search", msg => {
-  bot.sendMessage(msg.chat.id, "Loading...");
-  const name = msg.text.substr(msg.entities[0].length + 1);
+bot.onText(/\/search (.+)/, (msg, match) => {
+  const name = match[1];
 
   socket.emit("search", name, fb => {
     for (var i in fb) {
       const img = fb[i];
-      const startIndex = img.indexOf("/") + 1;
-      const fileName = "tmp." + img.substr(startIndex, img.indexOf(";") - startIndex);
-      fs.writeFile(fileName, img.substr(img.indexOf(",") + 1), "base64", err => {
+      const startIndex = img.image.indexOf("/") + 1;
+      const fileName = "tmp." + img.image.substr(startIndex, img.image.indexOf(";") - startIndex);
+      fs.writeFile(fileName, img.image.substr(img.image.indexOf(",") + 1), "base64", err => {
         if (err) throw err;
         bot.sendPhoto(msg.chat.id, fileName, {
-          caption: name
+          caption: img.name
         });
-        fs.unlink(fileName, err => {});
+        fs.unlink(fileName, err => {
+          if (err) throw err;
+        });
       });
     }
-    if (fb.length === 0) {
-      bot.sendMessage(msg.chat.id, "No photos with that name available");
-    }
+    if (fb.length === 0) bot.sendMessage(msg.chat.id, "No photos with that name available");
   });
 });
-
-bot.connect();
