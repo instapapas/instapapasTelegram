e9a9643e419ca46f234e69ff88e4d922925e02aa
ANyanCatFan <saitam.spatz@gmail.com>
2017-05-24 20:01:05 +0200

    New inline mode for searching the pictures directly in other chats Adapted to work with the firebase storage thing.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.API_KEY, {
   polling: true
 });
-//const socket = require('socket.io-client').connect("http://localhost:3000");
-const socket = require('socket.io-client').connect("https://instapapas.herokuapp.com");
-const fs = require("fs");
+//const socket = require("socket.io-client").connect("http://localhost:3000");
+const socket = require("socket.io-client").connect("https://instapapas.herokuapp.com");
+
+bot.onText(/(\/help|\/start)/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, "[insert help here]");
+});
 
 bot.onText(/\/search (.+)/, (msg, match) => {
   const name = match[1];
-
   socket.emit("search", name, fb => {
     for (var i in fb) {
-      const img = fb[i];
-      const startIndex = img.image.indexOf("/") + 1;
-      const fileName = "tmp." + img.image.substr(startIndex, img.image.indexOf(";") - startIndex);
-      fs.writeFile(fileName, img.image.substr(img.image.indexOf(",") + 1), "base64", err => {
-        if (err) throw err;
-        bot.sendPhoto(msg.chat.id, fileName, {
-          caption: img.name
-        });
-        fs.unlink(fileName, err => {
-          if (err) throw err;
-        });
+      bot.sendPhoto(msg.chat.id, fb[i].url, {
+        caption: fb[i].name
       });
     }
     if (fb.length === 0) bot.sendMessage(msg.chat.id, "No photos with that name available");
+  });
+});
+
+bot.on("inline_query", msg => {
+  var results = [];
+  socket.emit("search", msg.query, fb => {
+    for (var i in fb) {
+      const img = fb[i];
+      results.push({
+        "id": msg.id + i,
+        "type": "photo",
+        "caption": img.name,
+        "photo_url": img.url,
+        "thumb_url": img.url
+      });
+    }
+    bot.answerInlineQuery(msg.id, results);
   });
 });
